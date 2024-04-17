5d8c3b7f97b610a8de72951e929dcfe40cdf7295
Alberto Formaggio <alberto.formaggio@studenti.unipd.it>
2024-04-17 22:59:35 +0200

    Added hamburger menu

--- scripts/hamburger_animation.js
+++ scripts/hamburger_animation.js
@@ -1,13 +1,9 @@
-<body>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script>
-        $('.hamburger').click( function(){
-            $('.hamburger').toggleClass('active');
-            $('.hamburger-menu').toggleClass('active');
-    });
+$('.hamburger').click( function(){
+    $('.hamburger').toggleClass('active');
+    $('.hamburger-menu').toggleClass('active');
+});
 
-        $(window).load(function() {
-        $('body').addClass('loaded');
-    });
-    </script>
-</body>
+$(window).load(function() {
+$('body').addClass('loaded');
+});
+
